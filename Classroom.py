75a9ee41619fd5a63ac0dbb857264546657b62af
SkrrtSkrrt652 <34437907+SkrrtSkrrt652@users.noreply.github.com>
2021-06-27 03:26:25 +0530

    Add files via upload

--- Classroom.py
+++ Classroom.py
@@ -15,6 +15,24 @@ class Attendance():
         self.attendees = set()
         self.vc_attendance = dict()
     
+class PopQuiz():
+    def __init__(self):
+        self.optionEmojis='🔵🔴🟢🟡⚪⚫🟣🟤'
+        self.questions = []
+        self.answers = dict()
+        self.quizMessage = None
+        self.current_qn = None
+        self.thrown = 0
+        self.total = 0
+        self.correct = 0
 
+    def parse(self, file):
+        for line in file:
+            print(line)
+            parts = line.split(',')
+            question = parts[0]
+            ans_index = int(parts[-1])
+            options = [option for option in parts[1:-1]]
+            self.questions.append((question, tuple(options), ans_index))
 
     
