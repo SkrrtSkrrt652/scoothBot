fb05ad02522f28dc12b6d6b3cdfc7b0aabc96b99
Kevin Jacob <38008146+kevinthetesla@users.noreply.github.com>
2021-06-26 20:51:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,62 +2,69 @@ import discord
 import requests
 import json
 import time
+import cringe
+from discord.ext.commands import Bot
+from discord.ext import commands
+
 import search
-client = discord.Client()
 
-url = "https://random-stuff-api.p.rapidapi.com/joke/any"
+bot = Bot(command_prefix='$')
 
-querystring = {"api_key":"TctfASVGKw6P"}
+categories = ['artliterature', 'language', 'sciencenature', 'general', 'fooddrinkn', 'peopleplaces', 'geography',
+                  'historyholidays', 'entertainment', 'toysgames', 'music mathematics', 'religionmythology',
+                  'sportsleisure']
 
-headers = {
-    'x-api-key': "TctfASVGKw6P",
-    'x-rapidapi-key': "8d718b9663msh96c9ff6652097dbp1d7662jsn93807b8c1a1c",
-    'x-rapidapi-host': "random-stuff-api.p.rapidapi.com"
-    }
+@bot.command()
+async def _help(ctx):
+    await ctx.send("format : $question <CATEGORY> <NUMBER OF QUESTIONS>\n Example : $question sciencenature 3")
+    await ctx.send("Available Categories :")
+    list = ""
+    global categories
+    for category in categories:
+        list += "\t\t" + category + "\n"
+    await ctx.send(list)
 
 
-@client.event
+@bot.command()
+async def question(ctx, question, limit):
+    global categories
+    # try:
+    if limit.isnumeric() and categories.count(question):
+        def check(msg):
+            return msg.channel == ctx.channel and msg.author == ctx.author
+        get_question = search.question()
+        for i in range(int(limit)):
+            q = get_question.get_questions(category=str(question))
+            if not q == None:
+                await ctx.send(q['question'])
+                msg = await bot.wait_for("message",check=check)
+                msg_list = []
+                try:
+                    msg_list.append(q["answer"].split().lower())
+                except:
+                    msg_list.append(q['answer'].lower)
+                print(msg_list)
+                if str(q['answer']).lower() == str(msg.content).lower() or msg_list.count(msg.content.lower):
+                    await ctx.send("Woohoo! Thats the right ans! 😄") if str(q['answer']).lower() == str(msg.content).lower() else await ctx.send("Thats right, but the complete ans is : "+ q['answer'] +" 😄")
+                else:
+                    await ctx.send("The answer is "+ q["answer"]+" 🥺")
+    elif categories.count(question):
+        await ctx.send("Category not available, heres some help")
+        await _help(ctx)
+    else:
+        await ctx.send("Dammit dude stick to them rules")
+    # except:
+    #     await ctx.send("Incorrect usage enter '$_help' for help \nCorrect usage : $question <CATEGORY> <NUMBER OF QUESTIONS>...")
+Cyringe = cringe.CringeFinder
+@bot.command()
+async def cringe(ctx):
+    msg = Cyringe.cringe(ctx)
+    await ctx.send(msg[0])
+    time.sleep(2)
+    await ctx.send(msg[1])
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-mssg_json = json.loads("{}")
-
-@client.event
-async def on_message(message):
-
-    categories = ['artliterature', 'language', 'sciencenature', 'general', 'fooddrinkn', 'peopleplaces', 'geography', 'historyholidays', 'entertainment', 'toysgames', 'music mathematics', 'religionmythology', 'sportsleisure']
-
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-    elif message.content.startswith('$Changes'):
-        await message.channel.send('Nothing useful')
-    elif message.content.startswith('$random'):
-        flag = False
-        while not flag:
-            response = requests.request("GET", url, headers=headers, params=querystring)
-            mssg_json = json.loads(response.text)
-            flags = json.loads(json.dumps(mssg_json["flags"]))
-            if not flags["nsfw"] and not flags["religious"] and not flags["racist"] and not flags["sexist"]:
-                await message.channel.send(mssg_json["setup"])
-                time.sleep(2)
-                await message.channel.send(mssg_json["delivery"])
-                flag = True
-    elif message.content.startswith("$help questions"):
-        await message.channel.send("format : $question <CATEGORY> <NUMBER OF QUESTIONS>\n Example : $question sciencenature 3")
-        await message.channel.send("Available Categories :")
-        list = ""
-        for category in categories:
-            list += "       "+category + "\n"
-        await message.channel.send(list)
-    elif message.content.startswith("$question"):
-        input = message.content.split()
-        if len(input) == 3 and categories.count(input[1]) and input[2].isnumeric():
-            get_questions = search.question()
-            for i in range(int(input[2])):
-                question = get_questions.get_questions(input[1])
-                await message.channel.send(question['question'])
-                if
-client.run('ODU3NTU2ODAwMDAxNTQwMTE3.YNRUAQ.4ikv-ocvTyn5st4il6ftZb_j2a8')
+bot.run('ODU3NTU2ODAwMDAxNTQwMTE3.YNRUAQ.1A58a0mRmijPsB5r16My08hgg5A')
